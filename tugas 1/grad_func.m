e4d369f8d70060456678f0a0ab82ac1b1dcc9b5d
Zhafir Wisam Septiana <109527340+WiseMann64@users.noreply.github.com>
2025-12-19 20:52:05 +0700

    Delete tugas 1 directory

--- tugas 1/grad_func.m
+++
@@ -1,9 +0,0 @@
-function out = grad_func(f,x,dx)
-    out = zeros(size(x));
-    s = size(x,1);
-    for i=1:s
-        A = zeros([s,1]);
-        A(i,1) = 1;
-        out(i,1) = (f(x + A*dx) - f(x - A*dx))/(2*dx);
-    end
-end
